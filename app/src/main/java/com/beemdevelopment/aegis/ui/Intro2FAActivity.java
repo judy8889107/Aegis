9e3510f53f7170ecaf8393fd9ed8b90a5f9bdfbc
kelly <kelly7977@gmail.com>
2022-07-25 00:32:09 +0800

    shareActivity新增

--- app/src/main/java/com/beemdevelopment/aegis/ui/Intro2FAActivity.java
+++ app/src/main/java/com/beemdevelopment/aegis/ui/Intro2FAActivity.java
@@ -1,19 +1,12 @@
 package com.beemdevelopment.aegis.ui;
 
-import static com.beemdevelopment.aegis.ui.slides.SecurityPickerSlide.CRYPT_TYPE_BIOMETRIC;
 import static com.beemdevelopment.aegis.ui.slides.SecurityPickerSlide.CRYPT_TYPE_INVALID;
 import static com.beemdevelopment.aegis.ui.slides.SecurityPickerSlide.CRYPT_TYPE_NONE;
-import static com.beemdevelopment.aegis.ui.slides.SecurityPickerSlide.CRYPT_TYPE_PASS;
-
 import android.os.Bundle;
 import android.view.inputmethod.InputMethodManager;
-
-import com.beemdevelopment.aegis.R;
 import com.beemdevelopment.aegis.ThemeMap;
-import com.beemdevelopment.aegis.ui.dialogs.Dialogs;
 import com.beemdevelopment.aegis.ui.intro.IntroBaseActivity;
 import com.beemdevelopment.aegis.ui.intro.SlideFragment;
-import com.beemdevelopment.aegis.ui.slides.DoneSlide;
 import com.beemdevelopment.aegis.ui.slides.Intro2FA_first;
 import com.beemdevelopment.aegis.ui.slides.Intro2FA_five;
 import com.beemdevelopment.aegis.ui.slides.Intro2FA_four;
@@ -23,13 +16,6 @@ import com.beemdevelopment.aegis.ui.slides.Intro2FA_second;
 import com.beemdevelopment.aegis.ui.slides.Intro2FA_third;
 import com.beemdevelopment.aegis.ui.slides.Intro2FA_three;
 import com.beemdevelopment.aegis.ui.slides.Intro2FA_two;
-import com.beemdevelopment.aegis.ui.slides.SecurityPickerSlide;
-import com.beemdevelopment.aegis.ui.slides.SecuritySetupSlide;
-import com.beemdevelopment.aegis.ui.slides.WelcomeSlide;
-import com.beemdevelopment.aegis.vault.VaultFileCredentials;
-import com.beemdevelopment.aegis.vault.VaultRepositoryException;
-import com.beemdevelopment.aegis.vault.slots.BiometricSlot;
-import com.beemdevelopment.aegis.vault.slots.PasswordSlot;
 
 public class Intro2FAActivity extends IntroBaseActivity {
     @Override
