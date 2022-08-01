62acf3acee4afaf25a16a6a8ab40b43348efdaf5
kelly <kelly7977@gmail.com>
2022-08-01 22:05:41 +0800

    ShareActivity

--- app/src/main/java/com/beemdevelopment/aegis/ui/ShareActivity.java
+++ app/src/main/java/com/beemdevelopment/aegis/ui/ShareActivity.java
@@ -1,54 +1,28 @@
 package com.beemdevelopment.aegis.ui;
 
-import static com.mikepenz.iconics.Iconics.TAG;
-
-import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-
 import com.beemdevelopment.aegis.R;
 
 
-public class ShareActivity extends Activity {
+public class ShareActivity extends AegisActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_url_check);
 
-        Intent intent = getIntent();
-        intent.setClass(ShareActivity.this,UrlCheckActivity.class);
-        String receivedAction = intent.getAction();
-        String receivedType = intent.getType();
-        if (receivedAction.equals(Intent.ACTION_SEND)) {
 
-            // check mime type
-            if (receivedType.startsWith("text/")) {
+        // Get intent, action and MIME type
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+        sendIntent.setType("text/plain");
 
-                String receivedText = intent.getStringExtra(Intent.EXTRA_TEXT);
-                if (receivedText != null) {
-                    //do your stuff
-                    Intent intent1 = new Intent();
-                    intent1.setClass(ShareActivity.this,UrlCheckActivity.class);
-                    startActivity(intent1);
-                }
-            }
-
-            else if (receivedType.startsWith("image/")) {
-
-                Uri receiveUri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
-
-                if (receiveUri != null) {
-                    //do your stuff
-
-                }
-            }
-
-        } else if (receivedAction.equals(Intent.ACTION_MAIN)) {
-
-            Log.e(TAG, "onSharedIntent: nothing shared" );
-        }
+        Intent shareIntent = Intent.createChooser(sendIntent, null);
+        startActivity(shareIntent);
     }
+
 }
