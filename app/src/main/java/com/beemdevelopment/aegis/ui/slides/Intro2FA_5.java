6e2dde5f4fab343bd8ae0770e8b6e347b42952f0
kelly <kelly7977@gmail.com>
2022-08-21 21:38:15 +0800

    ShareActivity及dialog製作完成

---
+++ app/src/main/java/com/beemdevelopment/aegis/ui/slides/Intro2FA_5.java
@@ -0,0 +1,22 @@
+package com.beemdevelopment.aegis.ui.slides;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+
+import com.beemdevelopment.aegis.R;
+import com.beemdevelopment.aegis.ui.intro.SlideFragment;
+
+public class Intro2FA_5 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main_end, container, false);
+
+    }
+}
