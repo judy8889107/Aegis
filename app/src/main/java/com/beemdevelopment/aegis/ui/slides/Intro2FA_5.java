4461c9d910d909801be63eeaa54f3e47bbe45ef5
kelly <kelly7977@gmail.com>
2022-08-21 22:31:56 +0800

    imagebutton 跳轉

--- app/src/main/java/com/beemdevelopment/aegis/ui/slides/Intro2FA_5.java
+++ app/src/main/java/com/beemdevelopment/aegis/ui/slides/Intro2FA_5.java
@@ -27,6 +27,78 @@ public class Intro2FA_5 extends Fragment {
                 startActivity(intent);
             }
         });
+        view.findViewById(R.id.imageButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri=Uri.parse("https://support.apple.com/en-us/HT204915");
+                Intent intent=new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
+        view.findViewById(R.id.imageButton2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri=Uri.parse("https://www.facebook.com/help/148233965247823");
+                Intent intent=new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
+        view.findViewById(R.id.imageButton3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri=Uri.parse("https://help.dropbox.com/teams-admins/team-member/enable-two-step-verification");
+                Intent intent=new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
+        view.findViewById(R.id.imageButton4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri=Uri.parse("https://www.linkedin.com/help/linkedin/answer/544");
+                Intent intent=new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
+        view.findViewById(R.id.imageButton5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri=Uri.parse("https://safety.google/authentication/");
+                Intent intent=new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
+        view.findViewById(R.id.imageButton6).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri=Uri.parse("https://docs.microsoft.com/en-us/azure/active-directory/authentication/concept-mfa-howitworks");
+                Intent intent=new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
+        view.findViewById(R.id.imageButton7).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri=Uri.parse("https://help.twitter.com/en/managing-your-account/two-factor-authentication");
+                Intent intent=new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
+        view.findViewById(R.id.imageButton8).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri=Uri.parse("https://help.instagram.com/566810106808145");
+                Intent intent=new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
+        view.findViewById(R.id.imageButton9).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri=Uri.parse("https://2fa.directory/int/#");
+                Intent intent=new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
         return view;
     }
 }
