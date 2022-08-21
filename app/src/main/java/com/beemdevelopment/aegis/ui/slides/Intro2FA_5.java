ec77b1b505d24e8bfe333dad2fb0a37a7b9567b7
kelly <kelly7977@gmail.com>
2022-08-21 22:26:29 +0800

    imagebutton 跳轉

--- app/src/main/java/com/beemdevelopment/aegis/ui/slides/Intro2FA_5.java
+++ app/src/main/java/com/beemdevelopment/aegis/ui/slides/Intro2FA_5.java
@@ -1,6 +1,8 @@
 package com.beemdevelopment.aegis.ui.slides;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,7 +18,15 @@ public class Intro2FA_5 extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main_end, container, false);
-
+        View view = inflater.inflate(R.layout.fragment_main_end, container, false);
+        view.findViewById(R.id.imageButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri=Uri.parse("https://support.apple.com/en-us/HT204915");
+                Intent intent=new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
