9e8c566ccc47c3e002960e66a4c51dfe5c717be7
kelly <kelly7977@gmail.com>
2022-09-18 04:11:31 +0800

    新手導覽修改

--- app/src/main/java/com/beemdevelopment/aegis/ui/IntroUrlCheckActivity.java
+++ app/src/main/java/com/beemdevelopment/aegis/ui/IntroUrlCheckActivity.java
@@ -9,9 +9,10 @@ import androidx.viewpager.widget.ViewPager;
 import com.beemdevelopment.aegis.R;
 import com.beemdevelopment.aegis.ui.slides.BlankFragmentClass;
 import com.beemdevelopment.aegis.ui.slides.IntroUrlCheck_10;
-import com.beemdevelopment.aegis.ui.slides.IntroUrlCheck_11;
 import com.beemdevelopment.aegis.ui.slides.IntroUrlCheck_12;
 import com.beemdevelopment.aegis.ui.slides.IntroUrlCheck_13;
+import com.beemdevelopment.aegis.ui.slides.IntroUrlCheck_4_2;
+import com.beemdevelopment.aegis.ui.slides.IntroUrlCheck_4_3;
 import com.beemdevelopment.aegis.ui.slides.IntroUrlCheck_5;
 import com.beemdevelopment.aegis.ui.slides.IntroUrlCheck_1;
 import com.beemdevelopment.aegis.ui.slides.IntroUrlCheck_4;
@@ -19,6 +20,8 @@ import com.beemdevelopment.aegis.ui.slides.IntroUrlCheck_2;
 import com.beemdevelopment.aegis.ui.slides.IntroUrlCheck_3;
 import com.beemdevelopment.aegis.ui.slides.IntroUrlCheck_6;
 import com.beemdevelopment.aegis.ui.slides.IntroUrlCheck_7;
+import com.beemdevelopment.aegis.ui.slides.IntroUrlCheck_7_2;
+import com.beemdevelopment.aegis.ui.slides.IntroUrlCheck_7_3;
 import com.beemdevelopment.aegis.ui.slides.IntroUrlCheck_8;
 import com.beemdevelopment.aegis.ui.slides.IntroUrlCheck_9;
 import com.lwj.widget.viewpagerindicator.ViewPagerIndicator;
@@ -43,24 +46,27 @@ public class IntroUrlCheckActivity extends AppCompatActivity {
         viewPager = findViewById(R.id.viewpager);
         viewPagerIndicator = (ViewPagerIndicator) findViewById(R.id.viewPagerIndicator);
         //viewpager是固定页数, 传入viewpager即可
-        viewPagerIndicator.setViewPager(viewPager,13);
+        viewPagerIndicator.setViewPager(viewPager,16);
 
 
         //5.初始化三個Fragment分頁
-        fragments = new Fragment[13];
+        fragments = new Fragment[16];
         IntroUrlCheck_1 bf1 = new IntroUrlCheck_1();
         IntroUrlCheck_2 bf2 = new IntroUrlCheck_2();
         IntroUrlCheck_3 bf3 = new IntroUrlCheck_3();
         IntroUrlCheck_4 bf4 = new IntroUrlCheck_4();
-        IntroUrlCheck_5 bf5 = new IntroUrlCheck_5();
-        IntroUrlCheck_6 bf6 = new IntroUrlCheck_6();
-        IntroUrlCheck_7 bf7 = new IntroUrlCheck_7();
-        IntroUrlCheck_8 bf8 = new IntroUrlCheck_8();
-        IntroUrlCheck_9 bf9 = new IntroUrlCheck_9();
-        IntroUrlCheck_10 bf10 = new IntroUrlCheck_10();
-        IntroUrlCheck_11 bf11 = new IntroUrlCheck_11();
-        IntroUrlCheck_12 bf12 = new IntroUrlCheck_12();
-        IntroUrlCheck_13 bf13 = new IntroUrlCheck_13();
+        IntroUrlCheck_4_2 bf5 = new IntroUrlCheck_4_2();
+        IntroUrlCheck_4_3 bf6 = new IntroUrlCheck_4_3();
+        IntroUrlCheck_5 bf7 = new IntroUrlCheck_5();
+        IntroUrlCheck_6 bf8 = new IntroUrlCheck_6();
+        IntroUrlCheck_7 bf9 = new IntroUrlCheck_7();
+        IntroUrlCheck_7_2 bf10 = new IntroUrlCheck_7_2();
+        IntroUrlCheck_7_3 bf11 = new IntroUrlCheck_7_3();
+        IntroUrlCheck_8 bf12 = new IntroUrlCheck_8();
+        IntroUrlCheck_9 bf13 = new IntroUrlCheck_9();
+        IntroUrlCheck_10 bf14 = new IntroUrlCheck_10();
+        IntroUrlCheck_12 bf15 = new IntroUrlCheck_12();
+        IntroUrlCheck_13 bf16 = new IntroUrlCheck_13();
         //6.陣列內容
         fragments[0] = bf1;
         fragments[1] = bf2;
@@ -75,6 +81,9 @@ public class IntroUrlCheckActivity extends AppCompatActivity {
         fragments[10] = bf11;
         fragments[11] = bf12;
         fragments[12] = bf13;
+        fragments[13] = bf14;
+        fragments[14] = bf15;
+        fragments[15] = bf16;
 
         //7.初始化轉換器
         adapter = new BlankFragmentClass(getSupportFragmentManager(), fragments);
---
+++ app/src/main/java/com/beemdevelopment/aegis/ui/slides/IntroUrlCheck_7_3.java
@@ -0,0 +1,18 @@
+package com.beemdevelopment.aegis.ui.slides;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.beemdevelopment.aegis.R;
+
+public class IntroUrlCheck_7_3 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_urlcheck_7_3, container, false);
+    }
+}
